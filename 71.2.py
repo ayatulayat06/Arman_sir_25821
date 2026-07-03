36c6e7f4c65e556022c58ddf0c50be0219be365f
ayat <ayatulayat063@gmail.com>
2026-07-03 18:46:33 +0600

    digit count

---
+++ 71.2.py
@@ -0,0 +1,9 @@
+num = int (input("Enter a number"))
+temp = abs (num)
+sum_digit = 0
+while temp >0:
+    digit = temp %10
+    sum_digit += digit
+    temp=temp//10
+
+print("sum of digit is:", sum_digit)
---
+++ 74.py
@@ -0,0 +1,6 @@
+print("Printing 1 to 10 except the number 5:")
+
+for i in range(1,11):
+    if i ==5:
+        continue
+    print(i)
