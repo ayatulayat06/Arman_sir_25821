f292b65488a921a65ecdaad1bbd037060a3ef1da
ayat <ayatulayat063@gmail.com>
2026-07-05 21:36:45 +0600

    print_# Converting Integer to Float

---
+++ 017.py
@@ -0,0 +1,6 @@
+# Converting Integer to Float
+integer_number = 50
+print("orgginal number:", integer_number, "type:", type(integer_number))
+
+float_number = float(integer_number)
+print("float_num:", float_number, "type:", type(float_number))
