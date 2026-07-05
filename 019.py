f8ddf34de2888e9a86b95f2d9bd891feba1c3283
ayat <ayatulayat063@gmail.com>
2026-07-05 22:09:18 +0600

    print_# Converting numeric string to integer

---
+++ 019.py
@@ -0,0 +1,10 @@
+# Converting numeric string to integerteger
+num_str = "625"
+print("Original string:", num_str, type(num_str))
+
+
+# Convert to integer
+num_int = int(num_str)
+result = num_int + 25
+print("After conversion and adding 25:", result)
+print("New Type:", type(num_int))
