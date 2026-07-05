be7659f033ae51acc476b0b4a8c8d2366942854f
ayat <ayatulayat063@gmail.com>
2026-07-05 23:45:33 +0600

    print_# Demonstrating Comparison Operators

---
+++ 023.py
@@ -0,0 +1,11 @@
+# Demonstrating Comparison Operators
+
+x= int(input("Enter first number: "))
+y= int(input("Enter second number: "))
+
+print("x is equal to (x==y):", x==y)
+print("x is not equal to (x!=y):", x!=y)
+print("x is greater than (x>y):", x>y)
+print("x is less than (x<y):", x<y)
+print("x is greater than or equal to (x>=y):", x>=y)
+print("x is less than or equal to (x<=y):", x<=y)
