f5242777146ad9febba423a9a0fed9372b2a737c
ayat <ayatulayat063@gmail.com>
2026-07-05 22:14:10 +0600

    print_বিভ􀑂 ধরেনর Variable-এর Data Type 􀏕দশন􀎺 ক􁃈ন।

---
+++ 20.py
@@ -0,0 +1,8 @@
+age = 22 
+height = 5.8
+name = "Abdur Rahman"
+is_student = True 
+print("Value:", age, "| Type:", type(age))
+print("Value:", height, "| Type:", type(height))
+print("Value:", name, "| Type:", type(name))
+print("Value:", is_student, "| Type:", type(is_student))
