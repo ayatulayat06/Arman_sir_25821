01b91c0d85db463d4fda85851528f03d3f2af210
ayat <ayatulayat063@gmail.com>
2026-07-06 21:43:51 +0600

    print_# Practical use of modulus operator: Converting seconds to minutes and seconds

---
+++ 029.py
@@ -0,0 +1,6 @@
+# Practical use of modulus operator: Converting seconds to minutes and seconds
+total_seconds = int(input("Enter total seconds: "))
+minutes = total_seconds // 60
+remaining_seconds = total_seconds % 60
+print(total_seconds, "seconds is equal to:", minutes, "minutes and",
+remaining_seconds, "seconds")
