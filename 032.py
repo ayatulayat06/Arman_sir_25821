a14595b0b149a75e266822d97409c2c2fd9c3ae4
ayatulayat06 <ayatulayat063@gmail.com>
2026-07-08 00:38:09 +0600

    print_[200~# Finding perimeter of a rectangle

---
+++ 032.py
@@ -0,0 +1,5 @@
+# Finding perimeter of a rectangle
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+perimeter = 2 * (length + width)
+print("The perimeter of rectangle is:", perimeter)
