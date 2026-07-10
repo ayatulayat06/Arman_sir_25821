3b934e39a33182cb7a53b276df2e49aef725e783
Ayatul Ayat <ayatulayat06>
2026-07-10 06:34:18 +0600

    Update Python programs

---
+++ 36.py
@@ -0,0 +1,5 @@
+# Finding area of a triangle given base and height
+base = float(input("Enter base of triangle: "))
+height = float(input("Enter height of triangle: "))
+area = 0.5 * base * height
+print("The area of triangle is:", area)
