ee5d1ef250a881dbbef32808907bfb7e58472c5b
Ayatul Ayat <ayatulayat06>
2026-07-10 22:08:15 +0600

    # Finding grade based on marks

---
+++ 051.py
@@ -0,0 +1,19 @@
+# Finding grade based on marks 
+marks = float(input("Enter your marks (0-100): ")) 
+if marks < 0 or marks > 100: 
+    print("Invalid marks entered!")
+elif marks >= 90:
+    print("Grade: A+")
+elif marks >= 80:
+    print("Grade: A")
+elif marks >= 70:
+    print("Grade: A-")
+elif marks >= 60:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+elif marks >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
+    
