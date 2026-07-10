8df7d4c09b30911dcf7ae882950741acb98fb2ef
Ayatul Ayat <ayatulayat06>
2026-07-10 06:58:20 +0600

    print_# Converting Fahrenheit to Celsius

---
+++ 040.py
@@ -0,0 +1,4 @@
+# Converting Fahrenheit to Celsius 
+fahrenheit = float(input("Enter temperature in Fahrenheit: ")) 
+celsius = ((fahrenheit - 32) * 5) / 9 
+print("Temperature in Celsius is:", round(celsius, 2)) 
