b00079d50bead4f6475000a67be9f7b4c8578b56
Ayatul Ayat <ayatulayat06>
2026-07-10 07:40:11 +0600

    print_# Finding minimum between two numbers

---
+++ 047.py
@@ -0,0 +1,9 @@
+# Finding minimum between two numbers 
+num1 = float(input("Enter first number: ")) 
+num2 = float(input("Enter second number: ")) 
+if num1 < num2: 
+    print("Minimum number is:", num1) 
+elif num2 < num1: 
+    print("Minimum number is:", num2) 
+else: 
+    print("Both numbers are equal")
