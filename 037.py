2dbdf39f3dee0c771f2d71ef54e402b68846098a
Ayatul Ayat <ayatulayat06>
2026-07-10 06:45:29 +0600

    print_finding area of a right-angled triangle

---
+++ 037.py
@@ -0,0 +1,5 @@
+# Finding area of a right-angled triangle 
+base = float(input("Enter base: ")) 
+perpendicular = float(input("Enter perpendicular height: ")) 
+area = (base * perpendicular) / 2 
+print("The area of right-angled triangle is:", area) 
