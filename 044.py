92ea1ef3b3c264d43482227fe3aa7fdd79ecca9a
Ayatul Ayat <ayatulayat06>
2026-07-10 07:35:35 +0600

    print_# Checking if a number is Positive, Negative or zero

---
+++ 044.py
@@ -0,0 +1,8 @@
+# Checking if a number is Even or Odd
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
+
+
---
+++ 045.py
@@ -0,0 +1,8 @@
+# Checking if a number is Positive, Negative or Zero 
+num = float(input("Enter a number: ")) 
+if num > 0: 
+    print("The number is Positive") 
+elif num < 0: 
+    print("The number is Negative") 
+else: 
+    print("The number is Zero")
