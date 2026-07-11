8fa655976a71c5aa89a420259bc5bf6b21e8ecc1
Ayatul Ayat <ayatulayat06>
2026-07-11 11:48:53 +0600

    print_# Checking Uppercase or Lowercase

---
+++ 057.py
@@ -0,0 +1,8 @@
+# Checking Uppercase or Lowercase 
+char = input("Enter an alphabet character: ")
+if 'A' <= char <= 'Z':
+    print(char, "is an UPPERCASE character")
+elif 'a' <= char <= 'z':
+    print(char, "is a lowercase character")
+else:
+    print(char, "is not an alphabet character")
