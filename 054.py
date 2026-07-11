16e50e20219610ea4e6c14249e39ff9140687d33
Ayatul Ayat <ayatulayat06>
2026-07-11 11:37:01 +0600

    print_# Checking if a character is a Digit

--- 054.py
+++ 054.py
@@ -1,6 +1,7 @@
 # Checking if a character is an Alphabet (Manual Method) 
 char = input("Enter a single character: ") 
-if ('a' <= char <= 'z') or ('A' <= char <= 'Z'): 
-    print(char, "is an Alphabet") 
+
+if ('A' <= char <= 'Z'): 
+    print(char, "is an Alphabet")
 else: 
-    print(char, "is NOT an Alphabet")
+    print(char, "is NOT an Alphabet") 
---
+++ 055.py
@@ -0,0 +1,6 @@
+# Checking if a character is a Digit 
+char = input("Enter a single character: ")  
+if '0' <= char <= '9': 
+    print(char, "is a Digit") 
+else: 
+    print(char, "is NOT a Digit")
