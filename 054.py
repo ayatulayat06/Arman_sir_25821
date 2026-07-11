a3e5aec66673c53f6fd9b808617834fc12b8373a
Ayatul Ayat <ayatulayat06>
2026-07-11 10:44:51 +0600

    print_# Checking voting eligibility

---
+++ 054.py
@@ -0,0 +1,6 @@
+# Checking if a character is an Alphabet (Manual Method) 
+char = input("Enter a single character: ") 
+if ('a' <= char <= 'z') or ('A' <= char <= 'Z'): 
+    print(char, "is an Alphabet") 
+else: 
+    print(char, "is NOT an Alphabet")
