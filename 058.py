84efddbe6644e401debfb452e82a38bc6dd36246
Ayatul Ayat <ayatulayat06>
2026-07-11 11:57:46 +0600

    print_# Checking validity of a triangle given three sides

---
+++ 058.py
@@ -0,0 +1,9 @@
+# Checking validity of a triangle given three sides 
+a = float(input("Enter side 1: ")) 
+b = float(input("Enter side 2: ")) 
+c = float(input("Enter side 3: "))
+
+if (a + b > c) and (b + c > a) and (a + c > b):
+    print("Valid Triangle! A triangle CAN be formed.")
+else:
+    print("Invalid Triangle! A triangle CANNOT be formed.")
