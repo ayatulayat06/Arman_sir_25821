700a7079f056d4d173bd2d22f9b4aa2c88382cdb
Ayatul Ayat <ayatulayat06>
2026-07-12 21:40:51 +0600

    print_palindrome number

---
+++ 084.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+original_num = num
+reverse_num = 0
+
+while num > 0:
+    digit = num % 10          # শেষ digit বের করার জন্য 
+    reverse_num = reverse_num * 10 + digit
+    num //= 10                # শেষ digit বাদ দেয়ার জন্য
+
+if original_num == reverse_num:
+    print(original_num, "is a Palindrome Number")
+else:
+    print(original_num, "is NOT a Palindrome Number")
+ 
