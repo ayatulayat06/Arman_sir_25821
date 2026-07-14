45c8c7d99e302b117d175ff5896473e3ab69ae15
Ayatul Ayat <ayatulayat06>
2026-07-14 21:41:37 +0600

    print_Cheak the neon number

--- 086.py
+++ 086.py
@@ -5,9 +5,16 @@ squere = a**2
 temp = squere
 sum_digits = 0
 while temp >0:
-    digit = 0
+    digit = temp % 10
     sum_digits += digit 
     temp = temp//10
 
 
-if sum_digits
+if sum_digits == a:
+    print(a, "is a neon number")
+
+
+else:
+    print(a, "is NOT neon number")
+
+
